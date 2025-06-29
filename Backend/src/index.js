034a8d3ac515782fa20500b02346bafcdd22187c
Reethika1327 <reethika357@gmail.com>
2025-06-29 23:06:19 +0530

    Project completed

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -16,7 +16,11 @@ dotenv.config();
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
-app.use(express.json());
+//app.use(express.json());
+
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+
 app.use(cookieParser());
 app.use(
   cors({
