a32fe97aab3f4021163e9934e5fa6966480684d1
Reethika1327 <reethika357@gmail.com>
2025-06-29 16:38:23 +0530

    Project completed

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -29,10 +29,10 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../Frontend/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../Frontend", "dist", "index.html"));
   });
 }
 
